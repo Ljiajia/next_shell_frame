3c57fa7485b26a8ba435c6f411494c4ce1f07b56
雷佳佳 <44163545+Ljiajia@users.noreply.github.com>
2020-04-20 10:39:49 +0800

    Update apiAddress.js

--- api/apiAddress.js
+++ api/apiAddress.js
@@ -1,28 +1,18 @@
 import { target } from '../package.json'
 //dev
-const DEV = 'https://beta-newscms.valicn.com'
-//beta
-const BETA = 'https://beta-newscms.valicn.com'
+const DEV = 'https://.........com' 
 // dawn
-const DAWN = 'https://api-cms.gbc.team'
-//release
-const RELEASE = 'https://api-cms.gbc.team'
+const DAWN = 'https://.......team' 
 
 //chainBrowerSSET
 let HTTPS
 switch (target) {
     case 'dev':
         HTTPS = DEV
-        break
-    case 'beta':
-        HTTPS = BETA
-        break
+        break 
     case 'dawn':
         HTTPS = DAWN
-        break
-    case 'release':
-        HTTPS = RELEASE
-        break
+        break 
 }
 
 const HTTP_BASE = HTTPS
